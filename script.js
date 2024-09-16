4279196e96f59842a56c1dec5ec1e7539509cce0
gabriel carreno <carrenocarlosgabriel@gmail.com>
2024-09-16 13:20:02 -0300

    some changes in css and js

--- script.js
+++ script.js
@@ -10,11 +10,13 @@ document.getElementById('contact-form').addEventListener('submit', function(even
     const generalEnquiry = document.getElementById('query-general')
     const supportRequest = document.getElementById('query-support')
     const message = document.getElementById('message')
+    const terms = document.getElementById('terms')
 
     // call the validation function for each field.
     validateField(firstName, 'first-name-error')
     validateField(lastName, 'last-name-error')
     validateField(email, 'email-error')
+    validateField(terms, 'terms-error')
 
     validateFieldTextArea(message, 'message-error')
     
